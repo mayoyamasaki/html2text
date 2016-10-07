587381d22ecd51752de283bd26fa68ce2f11d3ab
mayo.yamasaki <mayo.yamasaki@gmail.com>
2016-10-07 14:37:16 +0900

    [Mod] refactor html2text

--- html2text.py
+++ html2text.py
@@ -1,16 +1,19 @@
+# Copyright 2016 Mayo Yamasaki. All Rights Reserved.
 #
-# Extract body and title text from Website URL
+# Licensed under the Apache License, Version 2.0.
 #
-
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# ==============================================================================
+import argparse
 import re
-import urllib.request
 from readability.readability import Document
-from html2text import html2text
+import sys
+import urllib.request
 
 
-def extract_content(url):
-    """ get text and title from website URL"""
-
+def url2html(url):
+    """get html from internet url"""
     headers = {
         "User-Agent" :  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) \
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
@@ -22,19 +25,25 @@ def extract_content(url):
         html = response.read()
 
     if html is None:
-        raise("[Error] Can't get HTML. {}".fomat(url))
+        raise Exception("Failed to get HTML from" +  str(url))
+    return html
 
+
+def html2body(html):
+    """ extract title and body from html"""
     d = Document(html)
-    title, body_html = d.short_title(), d.summary()
+    title, body = d.short_title(), d.summary()
+    return title + '\n' + body
 
-    #body = re.sub(r" +", " ", html2text(sanitize(body_html)))
-    body = html2text(body_html)
-    ## delete space and filter blank sentences.
-    #body = '\n'.join(
-    #    filter(lambda x: x != '',
-    #        map(lambda s: s.strip(), body.split('\n'))
-    #    )
-    #)
+
+def format_body(url):
+    body = re.sub(r" +", " ", html2text(sanitize(body_html)))
+    # delete space and filter blank sentences.
+    body = '\n'.join(
+        filter(lambda x: x != '',
+            map(lambda s: s.strip(), body.split('\n'))
+        )
+    )
     return (title, body)
 
 
@@ -67,24 +76,28 @@ def html2text(html):
 
 
 def main():
-    import argparse
-    import sys
     parser = argparse.ArgumentParser(
-            description="Scrape title and body text from URL.")
+            description="extract text from HTML for text processing.")
 
-    parser.add_argument('url',
-                        action='store',
-                        type=str,
-                        help='target url as string')
-    parser.add_argument('-d', '--destinaiton',
-                        action='store',
-                        nargs='?',
-                        default=sys.stdout,
-                        type=argparse.FileType('w'),
-                        help='title + body text output file path')
+    parser.add_argument('input',
+                        help="input file or url.")
+
+    parser.add_argument('-b', '--not-body-extract',
+                        action='store_false',
+                        help="not extract main body text from html using buriy's readability")
+
     args = parser.parse_args()
 
-    title, body = extract_content(args.url.rstrip('\n'))
+    if args.input.startswith('http://') or args.input.startswith('https://'):
+        html = url2html(argsa)
+    else:
+        html = sys.stdin
+
+    if args.body_extract:
+        html  = html2body(html)
+
+    body = format_body(body)
+
     args.destinaiton.write(title + '\n' + body)
 
 
